c766f502def624219c650812e54f630eeee736a1
Marcus622 <marcus.vix@tn.techstarter.de>
2024-12-14 23:19:34 +0100

    Update strings.py

--- Python.md/strings.py
+++ Python.md/strings.py
@@ -4,7 +4,7 @@ print(x[6:10])
 
 # INDEXING > Konzept zur Zuordnung von Nummern an einzelnen Bestandteilen einer Sequenz/Zeichenkette.
 # Ein Index wird immer in eckigen Klammmern [] angegeben. Mit : und eines Wertes z.B. 6 zählt Python bis zum Ende
-# durch. Er printet alles, was nach dem 
+# durch. Er printet alles, was nach dem angegebenen Wert steht. Gibt man einen Endwert ein, wird bis dahin gezählt.
 
 # In diversen Programmiersprachen bezeichnet „Tupel“ gemeinhin einen Listen-Datentyp, welcher über eine feste Länge 
 # verfügt und nach Definition nicht mehr verändert werden kann. Programmiersprachen wie bspw. Python, TypeScript, 
@@ -21,3 +21,4 @@ print(x[6:10])
 my_list = [10, 20, 30, 40]
 print(my_list[0])
 print(my_list[3])
+
