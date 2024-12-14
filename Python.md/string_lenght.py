488868c7e927ba1fba305a5f210f6738ee1a1803
Marcus622 <marcus.vix@tn.techstarter.de>
2024-12-14 23:19:39 +0100

    Create string_lenght.py

---
+++ Python.md/string_lenght.py
@@ -0,0 +1,15 @@
+# Länge eines string herausfinden.
+# FUNKTION > einfache Funktion zum Zählen der Bestandteile eines Objekts, z.B. der Buchstaben einer Zeichenkette.
+# Strings werden mit + verkettet, Elemente verschiedener Datentypen mit Komma.
+
+y = ("Dieser Satz ist recht lang. Wie lange ist er, wenn ich dies noch hinzufüge?")
+
+print(len(y))
+
+print("Der String mit der Variable y ist ")
+print("Der String mit der Variable y ist", len(y), "Zeichen lang.")
+
+# Buchstaben in Strings verändern. Strings sind unveränderliche Datentypen.
+
+x = "hallo"
+x[0] = "H"
