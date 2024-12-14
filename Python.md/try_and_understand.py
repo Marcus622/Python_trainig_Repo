6b0fcbd0295bda8228149507ce053c649c487412
Marcus622 <marcus.vix@tn.techstarter.de>
2024-12-15 00:12:16 +0100

    Update try_and_understand.py

--- Python.md/try_and_understand.py
+++ Python.md/try_and_understand.py
@@ -5,4 +5,4 @@ berufsfeld = "Data Analyst"
 weiterbildungsdauer = 18-3
 print("Hallo liebes Team von " + firmenname + ", ich möchte gerne mit euch in der Firma " + firmenname + " arbeiten. Ich würde mich über eine Antwort freuen.")
 print("Ich habe eine Weiterbildung zum " + berufsfeld + ", gemacht.")
-print(weiterbildungsdauer)
+print("Diese hat " + str(weiterbildungsdauer) + " Monate gedauert.")
