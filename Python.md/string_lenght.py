3bbcffa42734276584670eee2691d30f2d22d1b8
Marcus622 <marcus.vix@tn.techstarter.de>
2024-12-18 00:25:42 +0100

    Update string_lenght.py

--- Python.md/string_lenght.py
+++ Python.md/string_lenght.py
@@ -6,7 +6,7 @@ y = ("Dieser Satz ist recht lang. Wie lange ist er, wenn ich dies noch hinzufüg
 
 print(len(y))
 
-print("Der String mit der Variable y ist ")
+
 print("Der String mit der Variable y ist", len(y), "Zeichen lang.")
 
 # Buchstaben in Strings verändern. Strings sind unveränderliche Datentypen.
@@ -16,5 +16,30 @@ x = "hallo"
 x = "H" +x[1:]
 print(x)
 
+x = "Hallo"
+print(x*10)
+# Man Kann auch print("Hallo" * 10) verwenden.
+print("Hallo" + "10") # print(Hallo + 10) > Fehler
+# print("Hallo" + "10") > funktioniert, aber unveränderbar (falls Variable geändert werden soll,
+# reagiert Ausgabebefehl darauf nicht) 
+# > Deshalb umwandeln mit str()
+print("Hallo" + str(10))
 
+y = 100
+print("Hallo" + str(y))
 
+# Funktionen für strings
+# z.upper() > Konvertiert in GROSSBUCHSTABEN
+# z.lower() > Konvertiert in kleinbuchstaben
+
+z = "Hallo Welt, ich lerne Python"
+
+print(z.upper())
+
+print(z.split())    # Mit z.split() werden Wörter getrennt, mit print(z.split(',')) wird am 
+# Komma getrennt
+
+z = 'Ich liebe Python Tutorials <3'
+
+print(z.find('Python'))
+print(z[z.find('Python'):])
