1158a243b2fda2788b5b51d809815851a71f9fa1
Marcus622 <marcus.vix@tn.techstarter.de>
2024-12-27 23:50:55 +0100

    Create training_20241227.py

---
+++ Python/training_20241227.py
@@ -0,0 +1,23 @@
+#x = "in drei Tagen ist Sylvester, danach kommt Neujahr."
+#print(x.find("ist"))
+#print(x[(x.find("ist")):])
+
+#print("Hallo" * 10)
+
+#print(10/2)
+#print(10%2)
+
+#print(type(10/2))
+
+#print(9**0.5)
+
+#y = int(input("Wieviel ist dein Ausgangsgehalt? "))
+
+#x = y * 1.1
+
+#print(x)
+
+# euro = float(input("Wieviel Euro hast du? "))
+# baht = euro * 35.53
+# print("Toll, für deine" ,euro, "Euro bekommst Du" ,baht, " Baht.")
+
