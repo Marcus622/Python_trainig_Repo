33d3a8b2363a2dad9cf69594a4830da556b5c021
Marcus622 <marcus.vix@tn.techstarter.de>
2024-12-28 23:15:41 +0100

    Create schleifen.py

---
+++ schleifen.py
@@ -0,0 +1,10 @@
+# while-schleifen
+# > ermöglicht wiederholbare Anweisungen
+# > mit while vor allem, wenn Anzahl der Durchläufe anfangs unbekannt
+# > solange (while) Bedingung erfüllt ist.
+
+
+counter = 0
+
+while counter <= 5:
+    print(counter)
