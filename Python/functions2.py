8f6e9366a1e14cad8956dc0457b39e6d76469ef2
Marcus622 <marcus.vix@tn.techstarter.de>
2024-12-30 01:53:25 +0100

    Create functions2.py

---
+++ Python/functions2.py
@@ -0,0 +1,22 @@
+
+absender = input("gib den absender ein: ")
+
+
+
+def generate_newsletter(absender):
+    empfänger = input("gib den empfänger ein: ")
+    print(f"hallo {empfänger}")
+    print("")
+    print("Ich möchte dich über meine neue adresse informieren.")
+    print()
+    print("Musterstr. 23")
+    print("12345 Musterhausen")
+    print()
+    print("gruss")
+    print("")
+    print(f"{absender}")
+
+generate_newsletter(absender)
+generate_newsletter(absender)
+generate_newsletter(absender)
+generate_newsletter(absender)
