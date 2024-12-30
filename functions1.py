61e38cfe9d1da6a783a52839b3588c5315a541c6
Marcus622 <marcus.vix@tn.techstarter.de>
2024-12-30 01:53:41 +0100

    Create functions1.py

---
+++ functions1.py
@@ -0,0 +1,17 @@
+# 1. Schreibe eine Funktion, die den Benutzer nach seinem Namen fragt und diesen
+# auf der Konsole ausgibt.
+def namensabfrage():
+    name = input("Gib dein Namen an: ")
+    print(f"Du bist also {name}.")
+
+namensabfrage()
+
+# Definiere eine Funktion namens "square_root", die eine Zahl annimmt und sie hoch zwei zurückgibt
+def square_root(zahl):
+    return num **2 
+
+num = 5
+result = square_root(num)
+print(f"Das Quadrat von {num} ist {result}.")
+
+
