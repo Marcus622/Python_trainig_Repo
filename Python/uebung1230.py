82ae036f37d7384c259f3f8fba7e9556e11505d8
Marcus622 <marcus.vix@tn.techstarter.de>
2025-01-01 21:55:12 +0100

    Update uebung1230.py

--- Python/uebung1230.py
+++ Python/uebung1230.py
@@ -1,8 +1,42 @@
-a = 5
-b = 15
-c = 17
+# a = 5
+# b = 15
+# c = 17
 
-print(a < b or b > c) or not (c != b and b < a) 
+# print(a < b or b > c) or not (c != b and b < a) 
 # print(a > b or a <= b)
 # print(a != b and a > b)
 # print(not(a == b))
+
+# km_meilen = 0.621371
+# km = float(input("Gib die Km ein, die du umrechnen möchtest: "))
+# meilen = km_meilen * km
+# print(f"{km} Km sind {meilen} Meilen.")
+
+
+
+
+
+# In diesem Programm soll der Nutzer ein Datum eingeben und es soll wiedergegeben werden, ob an diesem Tag Unterricht
+# oder frei ist.
+# Als erstes formuliere ich die Eingabeaufforderung
+
+import datetime
+
+
+print("Gib das gewünschte Datum im Format jjjj. mm. tt ein: ")
+
+# jetzt muss ich die freien Tage definieren, die nach der Eingabe des Datums verglichen werden.
+
+Winterferien = 2024.12.24.- 2025.01.01
+Osterferien = 2025.04.18.- 2025.04.21
+Sommerferien = 2025.08.11.-2025.08.19
+Winterferien1 =  2025.12.24.-2026.01.01
+Karfreitag = 2025.04.18
+Ostermontag = 2025.04.21
+Tag_der_Arbeit = 2025.05.01
+Christi_Himmelfahrt = 2025.05.29
+Pfingstmontag = 2025.06.09
+T_d_d_e = 2025.10.03
+Reformationstag = 2025.10.31
+Weihnacht1 = 2025.12.25
+Weihnacht2 = 2025.12.26
