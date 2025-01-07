d40b42a225fa6730e4e41a519f94546ba2f4e90c
Marcus622 <marcus.vix@tn.techstarter.de>
2025-01-07 17:48:15 +0100

    Create class.py

---
+++ Python/Januar/20250103/class.py
@@ -0,0 +1,43 @@
+# Umsetzung UML Klassendiagramm
+
+
+class Auto():
+    def __init__(self, name, motor, kilometer):
+        self.name = name
+        self.motor = motor
+        self.kilometerzahl = kilometer
+
+    def fahren(self, km):
+        if self.restreichweite >= km:
+            self.kilometerzahl += km
+            self.restreichweite -= km
+            print(f"Auto fährt {km}km.")
+        else: 
+            print("Reichweite nicht mehr ausreichend!")
+
+# ------ Erbende Klasse: Verbrenner
+# Über super().__init__(<args>) kann der Konstruktor der Superklasse aufgerufen werden
+class Verbrenner(Auto):
+    def __init__(self, name, kilometer):
+        super().__init__(name, "Verbrenner", kilometer)
+        self.tankstand = 100
+
+    def tanken(self, km):
+        self.tankstand += km
+        print("Vollgetankt mit Brennstoff")
+
+# ----- Erbende Klasse: E_Auto
+class E_Auto(Auto):
+    def __init__(self, name, kilometer):
+        super().__init__(name, "Elektro", kilometer)
+        self.akkuladung = 100
+    
+    def tanken(self, km):
+        self.tankstand += km
+        print("Vollgetankt mit Ökostrom")
+
+my_e_car = E_Auto("Tesla Model X", 22210)
+my_combustion_car = Verbrenner("BMW m3", 13000)
+
+print(my_e_car.motor)
+print(my_combustion_car.motor)
