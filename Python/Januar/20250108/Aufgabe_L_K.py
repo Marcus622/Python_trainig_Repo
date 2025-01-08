106d253e64a3738ad75978992f891974e48a9f03
Marcus622 <marcus.vix@tn.techstarter.de>
2025-01-08 19:57:22 +0100

    Create Aufgabe_L_K.py

---
+++ Python/Januar/20250108/Aufgabe_L_K.py
@@ -0,0 +1,44 @@
+# Erstelle eine Klasse, die ein Haustier beschreibt. 
+
+class Animal:
+    def __init__(self, name, species, age, favourite_food):
+        
+        self.name = name
+        self.species = species
+        self.age = age
+        self.favourite_food = favourite_food
+        self.energy_level = 100
+    
+    def get_description(self):
+        return f" {self.name} is a {self.species} and {self.age} years old."
+    
+    def play_duration(self, duration):
+        energy_lost = 5 * duration
+        if self.energy_level < energy_lost:
+            print("Achtung, Tier stirbt")
+        
+        else:
+            self.energy_level -= energy_lost
+            print(f"{self.name} hat {duration} Minuten gespielt und hat jetzt noch einen Energielevel von {self.energy_level}.")
+
+
+    def feed(self, food):
+        if food == self.favourite_food:
+            self.energy_level += 30
+
+        else:
+            self.energy_level += 10
+        self.energy_level = min(self.energy_level, 100)
+        
+        if self.energy_level >= 70:
+            print("Du darst keine Katze mehr verfüttern.")
+
+
+pet1 = Animal(f"Brutus", "dog", 8, "Katzen")
+pet2 = Animal(f"Joe", "monkey", 20, "bananas")
+print(pet1.get_description())
+pet1.play_duration(10)
+pet1.feed("Katzen")
+print(f"Sein Level liegt bei {pet1.energy_level}.")
+# pet1.feed("Katzen")
+
