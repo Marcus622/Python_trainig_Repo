0f35796d85b12dbb2be295ff9280bbc7e65b8c2f
Marcus622 <marcus.vix@tn.techstarter.de>
2025-01-13 08:29:01 +0100

    Delete currency_calculator.py

--- Python/currency_calculator.py
+++
@@ -1,7 +0,0 @@
-# print("Herzlich willkommen zum Währungsrechner.")
-# print("")
-# euro = float(input("Gib einen Betrag in Euro ein: "))
-# dollar = 1.07 * euro
-# print(f"Der Betrag entspricht {dollar}.")
-
-
