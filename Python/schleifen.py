fec0ef31397f4537ef9e84c975423436baab3ce3
Marcus622 <marcus.vix@tn.techstarter.de>
2025-01-13 08:28:29 +0100

    Update schleifen.py

--- Python/schleifen.py
+++ Python/schleifen.py
@@ -79,12 +79,58 @@
 
 # for - Schleifen kombinieren
 
-todos = ["Swiebel", "Nabane"]
+# todos = ["Swiebel", "Nabane"]
 
-for _ in range(1):
-    newitem = input("Was möchtest du deiner Liste hinzufügen? ")
-    todos.append(newitem)
-    print("Meine Liste hat diese Elemente:")
+# for _ in range(1):
+#     newitem = input("Was möchtest du deiner Liste hinzufügen? ")
+#     todos.append(newitem)
+#     print("Meine Liste hat diese Elemente:")
 
-    for todo in todos: 
-        print(f" - {todo}")
+#     for todo in todos: 
+#         print(f" - {todo}")
+
+# while Schleifen
+
+# counter = 0
+
+# while counter <= 5:
+#     print(counter)
+#     counter +=1
+
+
+einkaufsliste = []
+
+# Artikel hinzufügen
+# Aktion -> hinzufügen, entfernen, anzeigen, beenden
+
+while True:
+    aktion = input("Möchtest Du einen Artikel hinzufügen, entfernen, oder die Liste anzeigen? (hin / ent / anz / bee)")
+
+    # hinzufügen
+    if aktion == "hin":
+        artikel = input("Welchen Artikel möchtest Du hinzufügen? ")
+        einkaufsliste.append(artikel)
+        print("Artikel wurde hinzugefügt")
+
+
+    # löschen
+    elif aktion == "ent":
+        artikel = input("Welchen Artikel möchtest Du entfernen? ")
+        if artikel in einkaufsliste:
+            einkaufsliste.remove(artikel)
+            print("Artikel gelöscht.")
+        else:
+            print("Artikel gibt es nicht.")
+
+    # anzeigen
+    elif aktion == "anz":
+        print("Deine Einkaufsliste:")
+        print(einkaufsliste)
+
+    # beenden
+    elif aktion == "bee":
+        print("Einkaufsliste beendet")
+        break
+
+    else:
+        print("Du musst das schon richtig eingeben.")
