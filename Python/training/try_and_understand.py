c5acd76fa6cba65e8cb531e6e735f3f11f11c48d
Marcus622 <marcus.vix@tn.techstarter.de>
2025-01-20 16:21:52 +0100

    Create try_and_understand.py

---
+++ Python/training/try_and_understand.py
@@ -0,0 +1,8 @@
+# Die Variablen müssen immer über der Formel stehen.
+
+firmenname = "Ecosia GmbH"
+berufsfeld = "Data Analyst"
+weiterbildungsdauer = 18-3
+print("Hallo liebes Team von " + firmenname + ", ich möchte gerne mit euch in der Firma " + firmenname + " arbeiten. Ich würde mich über eine Antwort freuen.")
+print("Ich habe eine Weiterbildung zum " + berufsfeld + ", gemacht.")
+print("Diese hat " + str(weiterbildungsdauer) + " Monate gedauert.")
