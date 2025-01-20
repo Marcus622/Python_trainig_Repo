d14546a71ec07511c727d5644ac27e5024beadea
Marcus622 <marcus.vix@tn.techstarter.de>
2025-01-20 16:19:45 +0100

    Create currency_calculator.py

---
+++ Python/training/currency_calculator.py
@@ -0,0 +1,7 @@
+# print("Herzlich willkommen zum Währungsrechner.")
+# print("")
+# euro = float(input("Gib einen Betrag in Euro ein: "))
+# dollar = 1.07 * euro
+# print(f"Der Betrag entspricht {dollar}.")
+
+
