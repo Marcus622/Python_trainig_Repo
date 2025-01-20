c7106e30a47740ddddd1748ab2134759b58f1c1e
Marcus622 <marcus.vix@tn.techstarter.de>
2025-01-20 16:20:08 +0100

    Create funktionen.py

---
+++ Python/training/funktionen.py
@@ -0,0 +1,17 @@
+
+# > wiederverwertbare Codeblöcke nit vordefinierten Funktionalitäten.
+# > Schnellzugriff
+
+# def begruessung(name, alter): # Parameter
+#     print("Hallo",name,"willkommen. Du bist",alter, "Jahre alt.")
+
+# begruessung("Marcus",35)  # > wenn funktion aufgerufen wird, wird das Argument übergeben. Für jeden Parameter muss ein Argument übergeben werden, 
+#                           # wenn kein Standardwert hinterlegt ist.
+
+
+def addieren(**daten):
+    """
+    Diese Funktion ist dafür da
+    """
+
+    pass
