33ec9bbfcece5ee14cdbc61a3b1dccc5f2951022
Marcus622 <marcus.vix@tn.techstarter.de>
2025-01-20 16:21:49 +0100

    Create strings.py

---
+++ Python/training/strings.py
@@ -0,0 +1,21 @@
+# STRINGS (str) > Zeichenketten, können Buchstaben, aber auch Zahlen, Leerstellen oder Sonderzeichen beinhalten.
+x = "Hallo Welt!!!"
+print(x[6:10])
+
+# INDEXING > Konzept zur Zuordnung von Nummern an einzelnen Bestandteilen einer Sequenz/
+# Zeichenkette.
+# Ein Index wird immer in eckigen Klammmern [] angegeben. Mit : und eines Wertes z.B. 6 zählt 
+# Python bis zum Ende durch. Er printet alles, was nach dem angegebenen Wert steht. 
+# Gibt man einen Endwert ein, wird bis dahin gezählt.
+
+
+#SLICING > Konzept zum zerlegen von Sequenzen/Strings anhand ihrer Indizes
+#        > ermöglicht z.B. isolieren einzelner Wörter
+# 1. BESTIMMTE STELLE BIS ENDE [_:]
+# 2. ANFANG BIS BELIEBIGE STELLE [:...]
+
+# Beispiel für Indexing
+my_list = [10, 20, 30, 40]
+print(my_list[0])
+print(my_list[3])
+
