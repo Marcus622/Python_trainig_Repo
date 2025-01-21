f63b5c8b5af829c9d391a229806ecfdf3ee87670
Marcus622 <marcus.vix@tn.techstarter.de>
2025-01-21 16:36:41 +0100

    Update test_app.py

--- Python/training/test_app.py
+++ Python/training/test_app.py
@@ -1,10 +1,11 @@
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name__) # Die app dient dazu, verschiedene vorab definierte Anfragen über einen vorbestimmten Port zu leiten. In unserem Fall Port 6060.
+# Über diese Schnittstelle greifen Programme auf bestimmte Funktionen oder Daten zu, ohne dass diesen der interne Code bekannt sein muss.
 
 
 # GET route
-@app.route("/")
+@app.route("/") # Wenn die Route / aufgerufen wird, gelangt man auf die Startseite.
 def home():
     return "Hello, World! This is a simple API running on Mac."
 
@@ -36,9 +37,9 @@ def hilfe_func():
 
 @app.route("/contact")
 def contact_func():
-    return "Nehmen Sie Kontakt mit unserem Support auf."
+    return "Nehmen Sie Kontakt mit unserem Support auf." 
 
 # Run the app
 if __name__ == "__main__":
     # Use a custom port (e.g., 8080) and bind to all interfaces
-    app.run(debug=True, host="0.0.0.0", port=6060)
+    app.run(debug=True, host="0.0.0.0", port=6060) # Die app.run(port:6060) wird verwendet, um eine Schnittstelle für diese Anfragen festzulegen.
