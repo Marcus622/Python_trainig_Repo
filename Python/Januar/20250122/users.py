2a8d9c5dee2252eea0f5ec9bfcaa22d3f2b9ebcc
Marcus622 <marcus.vix@tn.techstarter.de>
2025-01-22 20:02:27 +0100

    Create users.py

---
+++ Python/Januar/20250122/users.py
@@ -0,0 +1,22 @@
+# Ziel: Erstelle eine Flask-App, die auf GET-Routen reagiert und verschiedene Nutzeraktionen
+# simuliert.
+# Beschreibung:
+# Hintergrund: Du hast eine Liste von Nutzern mit id, name und email:
+# users = [
+
+# {"id": 1, "name": "Alice", "email": "alice@example.com"},
+# {"id": 2, "name": "Bob", "email": "bob@example.com"},
+# {"id": 3, "name": "Charlie", "email": "charlie@example.com"}
+# ]
+
+users = [
+    {"id": 1,
+     "name": "Alice",
+     "email": "alice@example.com"},
+    {"id": 2,
+     "name": "Bob",
+     "email": "bob@example.com"},
+    {"id": 3,
+     "name": "Charlie",
+     "email": "charlie@example.com"},
+]
